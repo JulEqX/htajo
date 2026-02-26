5fc3a250d65e3e5a444c26aab6c9662d48c67d6d
Julian Chua <julianchua@toppanecquaria.com>
2026-02-26 21:46:03 +0800

    Initial commit

---
+++ src/data.ts
@@ -0,0 +1,42 @@
+export const letterContent = {
+    title: "Relationship Status Update",
+    date: new Date("2026-02-27"),
+    content: `Dear Joey ❤️,
+
+If you're reading this, it means that we've successfully and officially began our journey as a couple. To avoid spoiling your eyes with my awful handwriting, I've decided to write this letter here for you
+
+Even though we had our fair share of difficulties in the past year, God has been merciful to us and for that I am forever grateful. 
+
+As we pursue marriage, I pray that 
+
+Cherish every moment we have together
+
+With lots of love,
+Julian ❤️
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    `
+}
