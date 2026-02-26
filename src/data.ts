81ea518939f19973fce08eb58982b597d3b4bd9a
Julian Chua <julianchua@toppanecquaria.com>
2026-02-26 22:40:30 +0800

    2nd commit

--- src/data.ts
+++ src/data.ts
@@ -1,42 +1,17 @@
 export const letterContent = {
-    title: "Relationship Status Update",
+    title: "An Important Question",
     date: new Date("2026-02-27"),
-    content: `Dear Joey ❤️,
+    content: `Dearest Joey ❤️,
 
-If you're reading this, it means that we've successfully and officially began our journey as a couple. To avoid spoiling your eyes with my awful handwriting, I've decided to write this letter here for you
+As I’m reading this, we’re probably sitting across from one another with you right in front of me looking extra amazing and stunning in your green dress, staring at me with those sparkling eyes of yours. And to avoid spoiling those eyes with my awful handwriting, I’ve carefully crafted this digital letter for you. This letter has a very important question at the end of it, but before that there are things I would like to say. Though I probably have a million things to say right now, but alas I don’t want you to die of boredom on our first official date. Besides, we’ll probably have a lifetime to do it! So here goes!
 
-Even though we had our fair share of difficulties in the past year, God has been merciful to us and for that I am forever grateful. 
+Firstly, I’m sorry for how things started out between us, and taking a whole year to finally ask you out, but at the same time, I’m also glad we didn’t rush into things like how the world does it and allowed God’s perfect timing to take place. Even in the face of difficulties over the past year, I’ve been praying and thanking Him everyday for allowing our lives to cross paths, for us to grow and know each other better, and even now to journey together. God has been so patient and merciful to the both of us over the past year and even in moments where we least expected it.
 
-As we pursue marriage, I pray that 
+Thank you for being committed to me throughout the year and for putting up with my nonsense and goofiness. But more importantly, thank you for being the way you are, and accepting me for who I am, I know I haven’t been the perfect person you thought I would be, but I would like to try my very best to be respectful and honoring to you and your relationship with God. You are incredibly considerate of other people’s feelings and well-being, fun and honest to talk to, and just great to be around and do life with! I am so grateful to have met someone who genuinely love God and who put God in the center of her life, and I’m excited to see how much more you can grow to be more like Christ!
 
-Cherish every moment we have together
-
-With lots of love,
+I could go on and on all day about the myriad of things that I like about you, but I would love to experience them all with you as we get date. I would like us to start dating with the intention to marry and pursue and journey with God together in purity, and to experience what God has in store for us as a couple — both the good and the bad days. 
+So what do you say, would you like to take this step of faith with me and be my girlfriend?
+    
 Julian ❤️
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    `
+`
 }
